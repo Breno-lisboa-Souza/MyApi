24b70abbba07dc72e641282eb1046b6816a77f7a
Breno-lisboa-Souza <brenolisboa2004@gmail.com>
2023-06-26 16:39:43 -0300

    26/06

--- Program.cs
+++ Program.cs
@@ -7,10 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(options =>
 {
-   options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSomee"));
+   options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoLocal"));
 });
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+.AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.DateFormatString = "dd/MM/yyyy";
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
